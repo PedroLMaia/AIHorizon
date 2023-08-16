6e2a64b804a7f25e860e3eb3c3f4b20760b40b4e
PedroLMaia <pedro-luiz-maia@hotmail.com>
2023-08-16 17:43:23 -0300

    add navbar to mobile and fix sidebar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,14 +1,11 @@
-import { Menu } from "lucide-react";
 import { UserButton } from "@clerk/nextjs";
 
-import { Button } from "@/components/ui/button"
+import MobileSidebar from "./mobile-sidebar";
 
 const Navbar = () => {
     return (
         <div className="flex items-center p-4">
-            <Button variant="ghost" size="icon" className="md:hidden">
-                <Menu />
-            </Button>
+            <MobileSidebar />
             <div className="flex w-full justify-end">
                 <UserButton afterSignOutUrl="/" />
             </div>
--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -2,7 +2,10 @@
 
 import Image from "next/image";
 import Link from "next/link";
+import { Montserrat } from "next/font/google";
+import { usePathname } from "next/navigation";
 
+import { cn } from "@/lib/utils";
 import { 
     LayoutDashboard, 
     MessagesSquare, 
@@ -12,10 +15,6 @@ import {
     Code,
     Settings
 } from "lucide-react";
-
-import { Montserrat } from "next/font/google";
-
-import { cn } from "@/lib/utils";
 
 const montserrat = Montserrat({
     weight: "600",
@@ -63,10 +62,11 @@ const routes = [
         label: "Configurações",
         icon: Settings,
         href: "/configuracoes",
-    },
+    }
 ]
 
 const Sidebar = () => {
+    const pathname = usePathname();
     return (
         <div className="space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white">
             <div className="px-3 py-2 flex-1">
@@ -87,8 +87,10 @@ const Sidebar = () => {
                         <Link
                             href={route.href}
                             key={route.href}
-                            className="text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition"
-                        >
+                            className={cn("text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition",
+                            pathname === route.href ? "text-white bg-white/10" : "text-zinc-400"
+                            )}
+                            >
                             <div className="flex items-center flex-1">
                                 <route.icon className={cn("h-5 w-5 mr-3", route.color)} />
                                 {route.label}
