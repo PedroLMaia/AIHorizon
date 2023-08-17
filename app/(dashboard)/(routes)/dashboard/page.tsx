5761e68c19e361e2157a532bbb2afbe154ec766b
PedroLMaia <pedro-luiz-maia@hotmail.com>
2023-08-17 03:16:48 -0300

    add OpenAi API, user-avatar, loader, bot-avatar, chat route, chat page and deshboard

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,9 +1,17 @@
 "use client";
 
+import {
+  ArrowRight,
+  Code,
+  ImageIcon,
+  MessageSquare,
+  Music,
+  VideoIcon
+} from "lucide-react";
+import { useRouter } from "next/navigation";
+
 import { Card } from "@/components/ui/card";
 import { cn } from "@/lib/utils";
-
-import { ArrowRight, Code, ImageIcon, MessageSquare, Music, VideoIcon } from "lucide-react";
 
 const tools = [
   {
@@ -44,21 +52,23 @@ const tools = [
 ]
 
 const DashboardPage = () => {
+  const router = useRouter();
   return (
     <div>
       <div className="mb-8 space-y-4">
         <h2 className="text-2xl md:text-4xl font-bold text-center">
-          Descubra o poder da IA
+          Descubra o que a IA pode fazer
         </h2>
         <p className="text-muted-foreground font-light text-sm md:text-lg text-center">
-          Converse com a IA mais inteligente - Experimente o poder da IA
+          Converse com a IA mais sofisticada - Explore sua inteligência excepcional.
         </p>
       </div>
       <div className="px-4 md:px-20 lg:px-32 space-y-4">
         {tools.map((tool) => (
-          <Card 
-          key={tool.href}
-          className="p-4 border-black/5 flex items-center justify-between hover:shadow-md transition cursor-pointer"
+          <Card
+            onClick={() => router.push(tool.href)}
+            key={tool.href}
+            className="p-4 border-black/5 flex items-center justify-between hover:shadow-md transition cursor-pointer"
           >
             <div className="flex items-center gap-x-4">
               <div className={cn("p-2 w-fit rounded-md", tool.bgColor)}>
